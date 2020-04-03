2092f392511fc48b9d2d5c147f16242b58fff73b
Mya Pitzeruse <j.mya.pitz@gmail.com>
2020-04-03 18:06:39 -0500

    gh-3: added support for binding to multiple addresses

--- main.go
+++ main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"context"
-	"flag"
 	"log"
-	"net"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
+	"github.com/mjpitz/highlander-proxy/internal/config"
 	"github.com/mjpitz/highlander-proxy/internal/election"
 	"github.com/mjpitz/highlander-proxy/internal/election/k8s"
 	"github.com/mjpitz/highlander-proxy/internal/proxy"
 
 	"github.com/sirupsen/logrus"
+
+	"github.com/spf13/cobra"
 )
 
 func exitIff(err error) {
@@ -23,78 +24,110 @@ func exitIff(err error) {
 	}
 }
 
+var longDescription = `
+highlander-proxy is a simple to use, leader elected network proxy. It works by
+running alongside applications as a sidecar, ensuring that only one instance 
+handles traffic at a time. As soon as a new leader is elected, all connections 
+to the previous leader are terminated.
+
+To configure routes, use the the --routes flag. This flag can be provided
+multiple times for multiple port bindings. The format of the value is as such:
+
+  <bind-address>|<forward-address>[,<bind-address>|<forward-address>,...]
+
+Some examples of route configurations include:
+
+  tcp://0.0.0.0:8080|tcp://localhost:8080
+  tcp://0.0.0.0:8080|unix:///path/to/file
+`
+
 func main() {
-	protocol := "tcp"
 	identity := ""
-	bindAddress := ""
-	remoteAddress := ""
+	routes := config.RouteSlice{}
+	logLevel := "info"
+
+	// Kubernetes Configuration
+	kubeconfig := ""
 	lockNamespace := ""
 	lockName := ""
 	leaseDuration := 5 * time.Second
 	renewDuration := 2 * time.Second
 	retryPeriod := 1 * time.Second
-	kubeconfig := ""
-	logLevel := "info"
 
-	flag.StringVar(&protocol, "protocol", protocol, "The network protocol to operate on. Either 'tcp' or 'udp'.")
-	flag.StringVar(&identity, "identity", identity, "The identity of this process (ip:port).")
-	flag.StringVar(&bindAddress, "bind-address", bindAddress, "The address to bind the proxy to.")
-	flag.StringVar(&remoteAddress, "remote-address", remoteAddress, "The destination address.")
-	flag.StringVar(&lockNamespace, "lock-namespace", lockNamespace, "The namespace for the lock.")
-	flag.StringVar(&lockName, "lock-name", lockName, "The name of the lock within the namespace.")
-	flag.DurationVar(&leaseDuration, "lease-duration", leaseDuration, "The duration a lock is held.")
-	flag.DurationVar(&renewDuration, "renew-duration", renewDuration, "The duration between lock renewal.")
-	flag.DurationVar(&retryPeriod, "retry-period", retryPeriod, "The duration between retries.")
-	flag.StringVar(&kubeconfig, "kubeconfig", kubeconfig, "Location of the kubeconfig file.")
-	flag.StringVar(&logLevel, "log-level", logLevel, "Verbosity of the log messages.")
+	cmd := &cobra.Command{
+		Use:   "highlander-proxy",
+		Short: "A leader elected network proxy.",
+		Long:  longDescription,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			level, err := logrus.ParseLevel(logLevel)
+			exitIff(err)
 
-	flag.Parse()
+			logrus.SetLevel(level)
 
-	if identity == "" {
-		identity = bindAddress
+			root, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			electionConfig := &election.Config{
+				Context:       root,
+				Identity:      identity,
+				LockNamespace: lockNamespace,
+				LockName:      lockName,
+				LeaseDuration: leaseDuration,
+				RenewDeadline: renewDuration,
+				RetryPeriod:   retryPeriod,
+			}
+
+			leader, err := k8s.NewElector(electionConfig, kubeconfig)
+			exitIff(err)
+
+			stopCh := make(chan struct{})
+
+			for _, route := range routes.Routes() {
+				server := &proxy.Server{
+					Route:    route,
+					Identity: identity,
+					Leader:   leader,
+				}
+
+				err := server.Start(stopCh)
+				exitIff(err)
+			}
+
+			ch := make(chan os.Signal, 2)
+			signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+			go func() {
+				<-ch
+				logrus.Infof("received shutdown signal, terminating process")
+				close(stopCh)
+
+				<-ch
+				logrus.Infof("forcing termination of process")
+				os.Exit(1)
+			}()
+
+			select {
+			// let the workers be free
+			}
+		},
 	}
 
-	level, err := logrus.ParseLevel(logLevel)
+	flags := cmd.Flags()
+
+	// Route Configuration
+	flags.StringVar(&identity, "identity", identity, "The identity of this process.")
+	flags.Var(&routes, "routes", "Configures the underlying route table.")
+
+	// Debug Configuration
+	flags.StringVar(&logLevel, "log-level", logLevel, "Verbosity of the log messages.")
+
+	// Kubernetes Configuration
+	flags.StringVar(&kubeconfig, "kubeconfig", kubeconfig, "Location of the kubeconfig file.")
+	flags.StringVar(&lockNamespace, "lock-namespace", lockNamespace, "The namespace for the lock.")
+	flags.StringVar(&lockName, "lock-name", lockName, "The name of the lock within the namespace.")
+	flags.DurationVar(&leaseDuration, "lease-duration", leaseDuration, "The duration a lock is held.")
+	flags.DurationVar(&renewDuration, "renew-duration", renewDuration, "The duration between lock renewal.")
+	flags.DurationVar(&retryPeriod, "retry-period", retryPeriod, "The duration between retries.")
+
+	err := cmd.Execute()
 	exitIff(err)
-
-	logrus.SetLevel(level)
-
-	root, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	electionConfig := &election.Config{
-		Context:       root,
-		Identity:      identity,
-		LockNamespace: lockNamespace,
-		LockName:      lockName,
-		LeaseDuration: leaseDuration,
-		RenewDeadline: renewDuration,
-		RetryPeriod:   retryPeriod,
-	}
-
-	leader, err := k8s.NewElector(electionConfig, kubeconfig)
-	exitIff(err)
-
-	server := &proxy.Server{
-		Protocol:      protocol,
-		Identity:      identity,
-		RemoteAddress: remoteAddress,
-		Leader:        leader,
-	}
-
-	listener, err := net.Listen(protocol, bindAddress)
-	exitIff(err)
-
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-ch
-		logrus.Infof("received shutdown signal, terminating process")
-		cancel()
-		time.Sleep(time.Second)
-		_ = listener.Close()
-	}()
-
-	logrus.Infof("listening on %s://%s", protocol, bindAddress)
-	server.Serve(listener)
 }
