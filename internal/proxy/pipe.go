b0cae2fe921ed12e11cdc58ef5aed42bfa4076f4
Mya Pitzeruse <mjpitz@indeed.com>
2020-05-14 16:35:03 -0500

    proxy: cleaned up pipe function

--- internal/proxy/pipe.go
+++ internal/proxy/pipe.go
@@ -2,38 +2,37 @@ package proxy
 
 import (
 	"context"
-	"io"
-
 	"github.com/sirupsen/logrus"
+	"io"
 )
 
 // Pipe reads from the provider reader and writes data to the provider writer
 // until the underlying connections encounter an error.
 func Pipe(parent context.Context, reader io.ReadCloser, writer io.WriteCloser) {
 	ctx, cancel := context.WithCancel(parent)
-	defer cancel()
+	defer func() {
+		cancel()
+		_ = reader.Close()
+		_ = writer.Close()
+	}()
 
-	for done := false; !done; {
-		data := make([]byte, 256)
-
-		n, err := reader.Read(data)
-		if err != nil {
-			logrus.Tracef("encountered error reading from connection: %v", err)
-			break
-		}
-
-		if _, err := writer.Write(data[:n]); err != nil {
-			logrus.Tracef("encountered error writing to connection: %v", err)
-			break
-		}
-
+	for {
 		select {
 		case <-ctx.Done():
-			done = true
+			return
 		default:
+			data := make([]byte, 256)
+
+			n, err := reader.Read(data)
+			if err != nil {
+				logrus.Tracef("encountered error reading from connection: %v", err)
+				return
+			}
+
+			if _, err := writer.Write(data[:n]); err != nil {
+				logrus.Tracef("encountered error writing to connection: %v", err)
+				return
+			}
 		}
 	}
-
-	_ = reader.Close()
-	_ = writer.Close()
 }
